f4ffbc378426113850f2ab7cb497e507f21ba866
unknown <pabarnard@outlook.com>
2023-03-07 18:44:58 -0800

    Added first lecture code

---
+++ lectures/week1/first_python_demo.py
@@ -0,0 +1,15 @@
+print("Hello world!") # Note that we don't usually end a line of code with a semicolon ;
+
+# Comments start with the "#" symbol
+
+"""
+This is a multi-line
+comment (actually a docstring).
+"""
+
+# Variables are in snake_case in Python
+my_value = 20
+
+my_value = my_value * 2 # Statement to the right of the "=" is evaluated first, then stored in the variable name to the left of the "="
+
+my_value *= 2
---
+++ lectures/week1/js_demo.js
@@ -0,0 +1,4 @@
+console.log("Hello world!");
+
+// camelCase in JavaScript
+var myValue = 20; // let myValue = 20;
