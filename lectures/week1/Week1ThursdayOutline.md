478c3d21963680725b12f51e7073f1f5e127b43e
unknown <pabarnard@outlook.com>
2023-03-09 15:52:41 -0800

    Added Thursday office hour challenges for week 1

---
+++ lectures/week1/Week1ThursdayOutline.md
@@ -0,0 +1,26 @@
+# Python PT - outline for Week 1: Thursday, March 9, 2023
+
+## Reminders:
+- Please remember to get an early start with assignments and discussion topics every week!
+- Don't forget about the 20-minute rule and Dojo Hall!
+- If you still haven't successfully installed Python yet, please make sure you do so ASAP!
+- Please install MySQL Workbench and Server this weekend!
+- Remember to read the material BEFORE the lecture that day!
+
+## Core assignments this week:
+- For Loop Basic I
+- Nested Dictionaries and Lists
+
+Need at least 15 out of 16 done total this course to satisfy this requirement and to be eligible for the exam at the end of week 6!
+
+## Discussion topics reminder:
+- Due Sunday by 11:59 PM Pacific!
+
+Need at least 11 (recommended 12) out of 16 discussion topics to satisfy this requirement!
+
+## Outline for today:
+- More on loops - with lists
+- Dictionaries
+    - Accessing and manipulating values
+    - Looping through them
+- Combining data types (lists, dictionaries, etc.) - how to grab values and loop
---
+++ office_hours/week1/group_challenges_week_1.py
@@ -0,0 +1,42 @@
+"""
+Challenge 1:
+Create a loop that will print *every other* value in a list, starting with the value at index 0, index 2, etc.
+
+For example (feel free to make up your own lists - use your own variables and values!):
+my_list = [8, 1, "Hello", "green", True, 4, -1.5] # You should print 8, "Hello", True and -1.5 in this example
+# Your loop goes here.
+"""
+
+
+"""
+Challenge 2:
+Write a function that accepts a string as input and returns the number of vowels found in the string.
+For this exercise, "a", "e", "i", "o" and "u" are vowels; we will not count "y" for this challenge.
+If it helps, assume the string is all lower-case for now.  Then try to make it work regardless of if the
+letters are upper- or lower-case.
+
+For example: given "tommy", return 1 as "o" is a vowel, but "y" is not.  "adrian" should return 3.
+"""
+
+
+"""
+Challenge 3:
+Given a list of dictionaries as input in the format below, return a new list containing only the names.
+
+Format:
+some_variable = [
+    {
+        "name": "Adrian",
+        "favorite_food": "Pizza",
+        "favorite_number": 88
+    },
+    {
+        "name": "Jenny",
+        "favorite_food": "Sushi",
+        "favorite_number": 24
+    },
+]
+
+In this example, we should get ["Adrian", "Jenny"] as those are the names found.  Make this work for all lists,
+so please make up your own examples!
+"""
