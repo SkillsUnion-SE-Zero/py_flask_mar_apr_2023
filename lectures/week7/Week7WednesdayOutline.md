94cec7785aa012c43fc0cca057bc391ba2cdd4cc
unknown <pabarnard@outlook.com>
2023-04-19 15:49:59 -0700

    Added Wednesday week 7 office hour code and lecture outline

---
+++ lectures/week7/Week7WednesdayOutline.md
@@ -0,0 +1,23 @@
+# Python PT - outline for Week 7: Wednesday, April 19, 2023
+
+## Reminders:
+- For this course, you need the following to pass:
+    - Minimum 15 out of 16 core assignments done
+    - At least 11 out of 16 discussion topics done - this week's is due by 11:59 PM this Sunday night!
+    - Pass the belt exam with any belt (orange, red or black)
+- Don't forget about the 20-minute rule and Dojo Hall!
+
+## IMPORTANT announcements:
+- Make sure when you submit your Flask projects to submit your Pipfile and Pipfile.lock files!  When you submit projects that use a database, make sure you include your .mwb file!  For the exam, you will need to include these 3 files, along with the application itself!
+- Code from the belt review project will NOT be pushed to GitHub, and the same with the wireframe
+
+## Outline for today:
+- Finish building review project:
+    - Read one park with the user linked (the "view park" page)
+    - Read all parks with the users linked (the "parks" page)
+        - Conditional rendering of Edit and Delete links
+    - Read all the user's parks added (the "my parks" page)
+    - Update a park (the "edit park" page)
+    - Delete a park
+    - Adding conditions to ensure only the creator can edit/delete
+- At the end (if we have time): drop schema and test app fully again
---
+++ office_hours/week7/more_OOP_solutions.py
@@ -0,0 +1,69 @@
+"""
+This is starter code we'll use to create Ship and Sailor objects, which we'll also link.
+"""
+
+class Ship:
+    def __init__(self, data):
+        self.id = data["id"]
+        self.name = data["name"]
+        self.created_at = data["created_at"]
+        self.updated_at = data["updated_at"]
+
+
+class Sailor:
+    def __init__(self, data):
+        self.id = data["id"]
+        self.first_name = data["first_name"]
+        self.last_name = data["last_name"]
+        self.created_at = data["created_at"]
+        self.updated_at = data["updated_at"]
+
+sailor_results = [
+    {"id": 1, "first_name": "Adrian", "last_name": "Barnard", "created_at": "???", "updated_at": "???"},
+    {"id": 3, "first_name": "Jenny", "last_name": "Rocket", "created_at": "???", "updated_at": "???"},
+    {"id": 4, "first_name": "John", "last_name": "Doe", "created_at": "???", "updated_at": "???"},
+    {"id": 5, "first_name": "Penny", "last_name": "Lane", "created_at": "???", "updated_at": "???"},
+]
+
+ship_results = [
+    {"id": 1, "name": "USS Oregon", "created_at": "???", "updated_at": "???"},
+    {"id": 2, "name": "HMS London", "created_at": "???", "updated_at": "???"},
+    {"id": 3, "name": "USS Michigan", "created_at": "???", "updated_at": "???"},
+]
+
+one_sailor_with_ship_results = [
+    {
+        "id": 1, "first_name": "Adrian", "last_name": "Barnard", "created_at": "???", "updated_at": "???", 
+        "ships.id": 3, "name": "USS Michigan", "ships.created_at": "???", "ships.updated_at": "???"
+    }
+]
+
+sailors_with_ships_results = [
+    {
+        "id": 1, "first_name": "Adrian", "last_name": "Barnard", "created_at": "???", "updated_at": "???", 
+        "ships.id": 3, "name": "USS Michigan", "ships.created_at": "???", "ships.updated_at": "???"
+    },
+    {
+        "id": 3, "first_name": "Jenny", "last_name": "Rocket", "created_at": "???", "updated_at": "???",
+        "ships.id": 3, "name": "USS Michigan", "ships.created_at": "???", "ships.updated_at": "???"
+    },
+    {
+        "id": 4, "first_name": "John", "last_name": "Doe", "created_at": "???", "updated_at": "???",
+        "ships.id": 2, "name": "HMS London", "ships.created_at": "???", "ships.updated_at": "???"
+    },
+    {
+        "id": 5, "first_name": "Penny", "last_name": "Lane", "created_at": "???", "updated_at": "???",
+        "ships.id": 2, "name": "HMS London", "ships.created_at": "???", "ships.updated_at": "???"
+    },
+]
+
+one_ship_with_sailors_results = [
+    {
+        "id": 3, "name": "USS Michigan", "created_at": "???", "updated_at": "???",
+        "sailors.id": 1, "first_name": "Adrian", "last_name": "Barnard", "sailors.created_at": "???", "sailors.updated_at": "???"
+    },
+    {
+        "id": 3, "name": "USS Michigan", "created_at": "???", "updated_at": "???",
+        "sailors.id": 3, "first_name": "Jenny", "last_name": "Rocket", "sailors.created_at": "???", "sailors.updated_at": "???"
+    },
+]
